ea190ada672ec777676da09517e774d81689b3a2
kangjk <96231886+KangJKJK@users.noreply.github.com>
2024-10-13 19:32:48 +0900

    Update soniclabs.sh

--- soniclabs.sh
+++ soniclabs.sh
@@ -88,13 +88,13 @@ done)
 ];
 EOL
 
-    # 프록시 정보 입력 안내
-    echo -e "${RED}Civil을 피하기 위해 각 프라이빗키마다 하나씩 프록시가 필요합니다.${NC}"
-    echo -e "${YELLOW}프록시 정보를 입력하세요. 입력형식: http://proxyUser:proxyPass@IP:Port${NC}"
-    echo -e "${YELLOW}여러 개의 프록시는 쉼표로 구분하세요.${NC}"
-    echo -e "${YELLOW}프록시가 없다면 VPS IP로만 이용하게됩니다. 입력형식: http://root:패스워드@VPS IP:VPS port${NC}"
-    echo -e "${YELLOW}챗GPT를 이용해서 형식을 변환해달라고 하면 됩니다.${NC}"
-    read -p "프록시 정보를 입력하시고 엔터를 누르세요: " proxies
+# 프록시 정보 입력 안내
+echo -e "${RED}Civil을 피하기 위해 각 프라이빗키마다 하나씩 프록시가 필요합니다.${NC}"
+echo -e "${YELLOW}프록시 정보를 입력하세요. 입력형식: http://proxyUser:proxyPass@IP:Port${NC}"
+echo -e "${YELLOW}여러 개의 프록시는 쉼표로 구분하세요.${NC}"
+echo -e "${YELLOW}프록시가 없다면 VPS IP로만 이용하게됩니다. 입력형식: http://root:패스워드@VPS IP:VPS port${NC}"
+echo -e "${YELLOW}챗GPT를 이용해서 형식을 변환해달라고 하면 됩니다.${NC}"
+read -p "프록시 정보를 입력하시고 엔터를 누르세요: " proxies
 
 # proxy_list.js 파일에 프록시 정보 추가
 cat <<EOL > /root/soniclabs-arcade-bot/config/proxy_list.js
