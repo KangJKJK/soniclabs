6b509f08d3b5a6364449446b34b37617d450347f
kangjk <96231886+KangJKJK@users.noreply.github.com>
2024-11-06 23:11:40 +0900

    Update soniclabs.sh

--- soniclabs.sh
+++ soniclabs.sh
@@ -88,40 +88,20 @@ case $choice in
         echo "}"
     } > $WORK_DIR/config/config.js
     
-    # 프록시 정보 입력 안내
-    echo -e "${RED}Civil을 피하기 위해 각 프라이빗키마다 하나씩 프록시가 필요합니다.${NC}"
-    echo -e "${YELLOW}프록시 정보를 입력하세요. 입력형식: http://proxyUser:proxyPass@IP:Port${NC}"
-    echo -e "${YELLOW}여러 개의 프록시는 줄바꿈으로 구분하세요. 엔터를 두번 입력하면 입력이 끝납니다.${NC}"
+     # 프록시파일 생성
+    echo -e "${YELLOW}프록시 정보를 입력하세요. 입력형식: http://user:pass@ip:port${NC}"
+    echo -e "${YELLOW}여러 개의 프록시는 줄바꿈으로 구분하세요.${NC}"
+    echo -e "${YELLOW}입력을 마치려면 엔터를 두 번 누르세요.${NC}"
 
-    # 프록시 정보를 입력받기 위한 변수 초기화
-    proxies=""
-    empty_line_count=0  # 빈 줄 카운트 초기화
-
-    while IFS= read -r line; do
-        # 줄이 비어있으면 빈 줄 카운트 증가
-        if [[ -z "$line" ]]; then
-            ((empty_line_count++))  # 빈 줄 카운트 증가
-            # 빈 줄이 두 번이면 입력 종료
-            if [[ $empty_line_count -ge 2 ]]; then
-                break
-            fi
-        else
-            empty_line_count=0  # 빈 줄이 아니면 카운트 초기화
-            proxies+="$line"$'\n'  # 입력된 줄을 proxies 변수에 추가
-        fi
-    done
-
-    # 프록시를 배열로 변환
-    IFS=$'\n' read -r -a proxy_array <<< "$proxies"
-
-    # 결과를 proxy_list.js 파일에 저장
     {
-        echo "export const proxyList = ["
-        for proxy in "${proxy_array[@]}"; do
-            echo "    \"$proxy\","
+        echo "export const proxyList = ["  # 파일 시작
+        while IFS= read -r line; do
+            [[ -z "$line" ]] && break
+            # 입력된 프록시 정보를 그대로 사용
+            echo "  \"$line\","
         done
-        echo "];"
-    } > $WORK_DIR/config/proxy_list.js
+        echo "];"  # 배열 끝
+    } > "$WORK/config/proxiy_list.js"
     
     # 봇구동
     npm run start
